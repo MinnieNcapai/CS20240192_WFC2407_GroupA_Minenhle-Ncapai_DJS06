285b4d38bdd8d36e1640e7cf8bec5c091eeeb25d
Minenhle Ncapai <minipai64@gmail.com>
2024-11-09 18:14:58 +0200

    Include HTML file with JS link.  Add tasks for logging names/provinces and uppercase transformation
    - Log names and provinces, display matching names/provinces with forEach
    - Create uppercase provinces array with map and log it

--- scripts.js
+++ scripts.js
@@ -13,3 +13,17 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+//Task: Use forEach to log each name and each province to the console. Then, log each name with a matching province in the format "Name (Province)".
+// Logging each name and province
+console.log ("Names:", names)
+console.log ("Provinces:", provinces)
+// Logging each name with a matching province
+provinces.forEach((province,index) => {
+  console.log("Names with matching Provonces:",`${names[index]} (${province})`)
+})
+
+//Task:  Use map to create a new array of province names in all uppercase. Log the new array to the console.
+  //Uppercase Transformation
+  const uppercaseProvinces = provinces.map(province => province.toUpperCase())
+  console.log("Uppercased Province Names:", uppercaseProvinces);
